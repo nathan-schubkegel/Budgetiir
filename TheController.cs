945ad8f8cefd31d0a88476630735a05c1787c70d
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2023-12-01 21:16:45 -0800

    Some formatting

--- TheController.cs
+++ TheController.cs
@@ -60,6 +60,7 @@ namespace Budgetiir
         Envelopes = new []
         {
           new Envelope { Name = "Auto Insurance", SavedAmount = 0m, MonthlyGoal = new MonthlyGoal { DayOfMonth = 30, GoalAmount = 100m } },
+          new Envelope { Name = "Life Insurance", SavedAmount = 55m, MonthlyGoal = new MonthlyGoal { DayOfMonth = 1, GoalAmount = 51.33m } },
           new Envelope { Name = "Timothy Grass Purchase", SavedAmount = 100m, TargetDateGoal = new TargetDateGoal { StartDate = DateTime.Parse("2023-09-01T00:00:00Z"), TargetDate = DateTime.Parse("2024-03-01T00:00:00Z"), GoalAmount = 1500m } },
           new Envelope { Name = "Amazon Prime Subscription", SavedAmount = 50m, YearlyGoal = new YearlyGoal { DayOfYear = 100, GoalAmount = 170m } },
           new Envelope { Name = "Emergency Fund", SavedAmount = 600m, NoDeadlineGoal = new NoDeadlineGoal { GoalAmount = 1000m } },
